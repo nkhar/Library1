624db5cedf56a6ae4e5d2f68cd8f95c34eff728f
Nika <Nikoloz.Kharshiladze.2014@live.rhul.ac.uk>
2018-02-13 17:50:43 +0400

    DatabaseHelper class has all the functionality it needs so far, some methods and comments were not taken from DroidLibrary project, otherwise this class should be fully functional.

--- app/src/main/java/ge/apex/nika/library1/DatabaseHelper.java
+++ app/src/main/java/ge/apex/nika/library1/DatabaseHelper.java
@@ -6,9 +6,14 @@ package ge.apex.nika.library1;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
 
 import ge.apex.nika.library1.Data.Author;
 import ge.apex.nika.library1.Data.Book;
@@ -45,11 +50,86 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource) {
+        try {
+            Log.i(DatabaseHelper.class.getName(), "onCreateDatabaseHelper");
+            TableUtils.createTable(connectionSource, Author.class);
+            TableUtils.createTable(connectionSource, Book.class);
+            TableUtils.createTable(connectionSource, Genre.class);
+            TableUtils.createTable(connectionSource, IssuedBook.class);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
+            // throw new RuntimeException(e);
+        }
 
+        // here we try inserting data in the on-create as a test
+        // RuntimeExceptionDao<SimpleData, Integer> dao = getSimpleDataDao();
+        //  long millis = System.currentTimeMillis();
+        // create some entries in the onCreate
+        // SimpleData simple = new SimpleData(millis);
+        // dao.create(simple);
+        // simple = new SimpleData(millis + 1);
+        //   dao.create(simple);
+        //  Log.i(DatabaseHelper.class.getName(), "created new entries in onCreate: " + millis);
     }
 
+    /**
+     * This is called when your application is upgraded and it has a higher version number. This allows you to adjust
+     * the various data to match the new version number.
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            Log.i(DatabaseHelper.class.getName(), "onUpgrade");
+            TableUtils.dropTable(connectionSource, Author.class, true);
+            TableUtils.dropTable(connectionSource, Book.class, true);
+            TableUtils.dropTable(connectionSource, Genre.class, true);
+            TableUtils.dropTable(connectionSource, IssuedBook.class, true);
+            // after we drop the old databases, we create the new ones
+            onCreate(sqLiteDatabase, connectionSource);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't drop databases", e);
+            // throw new RuntimeException(e);
+        }
+    }
 
+    /** Create the getDao methods of all database tables to access those from android code.
+     Insert, delete, read, update everything will be happening through DAOs
+     */
+    public Dao<Author, Integer> getAuthorDao() throws SQLException {
+        if (authorDao == null) {
+            authorDao = getDao(Author.class);
+        }
+        return authorDao;
+    }
+
+    public Dao<Book, Integer> getBookDao() throws SQLException {
+        if (bookDao == null) {
+            bookDao = getDao(Book.class);
+        }
+        return bookDao;
+    }
+
+    public Dao<Genre, Integer> getGenreDao() throws SQLException{
+        if(genreDao == null) {
+            genreDao = getDao(Genre.class);
+        }
+        return genreDao;
+    }
+
+    public Dao<IssuedBook, Integer> getIssuedBookDao() throws SQLException{
+        if(issuedBookDao == null ) {
+            issuedBookDao = getDao(IssuedBook.class);
+        }
+        return issuedBookDao;
+    }
+
+    /**
+     * Close the database connections and clear any cached DAOs.
+     */
+    @Override
+    public void close() {
+        super.close();
+        authorDao = null;
+        bookDao = null;
     }
 }
