f149c2f56a2b7a4cd6920097d7d0f1b5af89b935
Nika <Nikoloz.Kharshiladze.2014@live.rhul.ac.uk>
2018-02-13 18:17:04 +0400

    Library activity implements navigatio drawer Authors tab which changes text in content textview with info taken from Database

--- app/src/main/java/ge/apex/nika/library1/LibraryActivity.java
+++ app/src/main/java/ge/apex/nika/library1/LibraryActivity.java
@@ -3,6 +3,7 @@ package ge.apex.nika.library1;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
+import ge.apex.nika.library1.Data.Author;
 
 public class LibraryActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Widgets
+    TextView mTextView;
+
+    protected final String LOG_TAG = "LibraryActivity";
+    protected final static int MAX_NUM_TO_CREATE = 8;
+
+    // Reference of DatabaseHelper class to access its DAOs and other componentspushing a
+    protected DatabaseHelper databaseHelper = null;
+
+    // Declaration of DAO to interact with corresponding Author table
+    protected Dao<Author, Integer> authorDao;
+    List<Author> authorList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +64,9 @@ public class LibraryActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // my code
+        mTextView = (TextView) findViewById(R.id.textview_display_database_tables);
     }
 
     @Override
@@ -81,7 +108,8 @@ public class LibraryActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_authors) {
-            // Handle the camera action
+            // Handle the authors action
+            doSampleAuthorStuff("AuthorStuff",mTextView);
         } else if (id == R.id.nav_books) {
 
         } else if (id == R.id.nav_genres) {
@@ -95,5 +123,91 @@ public class LibraryActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public DatabaseHelper getDatabaseHelper() {
+        if(databaseHelper == null ) {
+            databaseHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /**
+         *  You'll need this in your class to release the helper when done.
+		 */
+        if(databaseHelper != null ) {
+            OpenHelperManager.releaseHelper();
+            databaseHelper = null;
+        }
+    }
+
+    /**
+     * Do sample author stuff
+     */
+    protected void doSampleAuthorStuff(String action, TextView tv) {
+
+        try {
+
+            authorDao = getDatabaseHelper().getAuthorDao();
+            //List<SimpleData>
+            authorList = authorDao.queryForAll();
+            Log.d(LOG_TAG, "WE got authorDAO");
+
+            // our string builder for building the content-view
+            StringBuilder sb = new StringBuilder();
+            sb.append("Found ").append(authorList.size()).append(" entries in DB in ").append(action).append("()\n");
+
+            // if we already have items in the database
+            int authorC = 1;
+            for (Author author : authorList) {
+                sb.append('#').append(authorC).append(": ").append(author).append('\n');
+                authorC++;
+            }
+            sb.append("------------------------------------------\n");
+            sb.append("Deleted ids:");
+
+            for (Author author : authorList) {
+                authorDao.delete(author);
+                sb.append(' ').append(author.getId());
+                Log.i(LOG_TAG, "deleting author(" + author.getId() + ")");
+                authorC++;
+            }
+            sb.append('\n');
+            sb.append("------------------------------------------\n");
+
+            int createNum;
+            do {
+                createNum = new Random().nextInt(MAX_NUM_TO_CREATE) + 1;
+            } while (createNum == authorList.size());
+            sb.append("Creating ").append(createNum).append(" new entries:\n");
+            for (int i = 0; i < createNum; i++) {
+                // create a new simple object
+                long millis = System.currentTimeMillis();
+                // SimpleData simple = new SimpleData(millis);
+                Author author = new Author("Jack", "London", 1876);
+                // store it in the database
+                authorDao.create(author);
+
+                Log.i(LOG_TAG, "created author(" + millis + ")");
+                // output it
+                sb.append('#').append(i + 1).append(": ");
+                sb.append(author).append('\n');
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+            }
+
+
+            tv.setText(sb.toString());
+            Log.i(LOG_TAG, "Done with authors at " + System.currentTimeMillis());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
